db78ed9065c883ead345ceb2302518e0c670c8c8
user <user@DESKTOP-LCHG7PS.domain.name>
2022-08-13 11:35:19 +0530

    initial commit

---
+++ Hello-World/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
