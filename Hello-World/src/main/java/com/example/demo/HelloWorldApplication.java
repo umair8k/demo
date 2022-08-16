4039a9577e50049b37055a74a982f8047f7bfbce
user <user@DESKTOP-LCHG7PS.domain.name>
2022-08-16 23:13:10 +0530

    war file

--- Hello-World/src/main/java/com/example/demo/HelloWorldApplication.java
+++ Hello-World/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloWorldApplication.class);
+    }
 	
 
 	public static void main(String[] args) {
